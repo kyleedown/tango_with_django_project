b3c32b2f051e32e5de0851c78a75408f48d0972d
Kylee Down <3095263d@student.gla.ac.uk>
2025-02-11 12:41:44 +0000

    Chapter 6 fails test_response_markup

--- rango/views.py
+++ rango/views.py
@@ -4,9 +4,11 @@ from rango.models import Category, Page
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
+    page_list = Page.objects.order_by('-views')[:5]
     context_dict = {}
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
+    context_dict['pages'] = page_list
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
@@ -23,7 +25,7 @@ def show_category(request, category_name_slug):
     except Category.DoesNotExist:
         context_dict['category'] = None
         context_dict['pages'] = None
-        
+
     return render(request, 'rango/category.html', context=context_dict)
 
 
